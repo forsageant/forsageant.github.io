b88d342120bdf8dd7638cde32323fd913717c348
forsageant <forsageant@w3dex.net>
2023-09-27 10:55:12 +0800

    Update 202309271055

--- _next/static/dlSfXml4Z1V7scRLuV4U-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-709101233fcd2cb8.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
